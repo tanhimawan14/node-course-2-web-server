e475592d3836fbf2375b252a2dce758a21c98f57
tanhimawan14 <himawan.tan.b@gmail.com>
2017-03-27 18:23:24 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log.')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('dateYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    welcomeMessage: 'Hai, Welcome to some website',
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about', (req, res)=> {
+  res.render('about', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'This page error dude'
+  })
+});
+
+app.listen(3000), () => {
+  console.log('server on');
+};
